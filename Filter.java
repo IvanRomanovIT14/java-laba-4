222d0ceb8d0da5b75b86416b8da8a9b15bc9f52d
IvanRomanovIT14 <ivanromanov956@gmail.com>
2025-11-03 21:29:53 +0500

    Update Filter.java

--- Filter.java
+++ Filter.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Filter {
+    //Фильтрует элементы списка, возвращает список с элементами удовлетворяющими условию
     public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
         if (list == null) {
             return new ArrayList<>();
         }
-
         List<T> result = new ArrayList<>();
         for (T item : list) {
             if (item != null && predicate.test(item)) {
