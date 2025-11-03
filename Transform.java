a8e4794a54d8393dcc8bc2a9bd955a99be647704
IvanRomanovIT14 <ivanromanov956@gmail.com>
2025-11-03 21:29:28 +0500

    Update Transform.java

--- Transform.java
+++ Transform.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Transform {
+    //Преобразует элементы списка из типа T в тип R с помощью заданной функции.
     public static <T, R> List<R> transform(List<T> list, Function<T, R> function) {
         if (list == null) {
             return new ArrayList<>();
         }
-
         List<R> result = new ArrayList<>();
         for (T item : list) {
             if (item != null) {
