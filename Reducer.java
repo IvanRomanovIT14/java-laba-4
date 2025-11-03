e5c3e7a67f7518491a364be1285c935cf3071ca9
IvanRomanovIT14 <ivanromanov956@gmail.com>
2025-11-03 21:30:22 +0500

    Update Reducer.java

--- Reducer.java
+++ Reducer.java
@@ -1,12 +1,12 @@
 import java.util.List;
 
 public class Reducer {
-    public static <T> T reduce(List<T> list, Reduce<T> reducer, T initialValue) {
+    //Объединяет все элементы списка в один результат
+    public static <T> T reduce(List<T> list, Reduce<T> reducer, T start) {
         if (list == null || list.isEmpty()) {
-            return initialValue;
+            return start;
         }
-
-        T result = initialValue;
+        T result = start;
         for (T item : list) {
             if (item != null) {
                 result = reducer.reduce(result, item);
