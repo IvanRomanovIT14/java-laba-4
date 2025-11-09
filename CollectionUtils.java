cd89bc67ed4295e10eff32e19b6bc056b4250e61
IvanRomanovIT14 <ivanromanov956@gmail.com>
2025-11-09 21:03:53 +0500

    Update CollectionUtils.java

--- CollectionUtils.java
+++ CollectionUtils.java
@@ -4,11 +4,12 @@ public class CollectionUtils {
     public static <T, P> P collect(List<T> list, Collector<P, T> collector, Accumulator<P, T>
             accumulator) { //Создаёт коллекцию и добавляет в неё все элементы из списка
         if (list == null) {
-            return collector.createCollection();
+            return collector.createCollection(); //Возвращаем пустую коллекцию
         }
         P result = collector.createCollection();
         for (T item : list) {
             if (item != null) {
+                //Добавляем элемент в коллекцию с помощью аккумулятора
                 accumulator.accumulate(result, item);
             }
         }
