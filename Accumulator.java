f806b4a8a0ababaa0b42bc58d304c89bb2d36ed8
IvanRomanovIT14 <ivanromanov956@gmail.com>
2025-11-09 21:05:57 +0500

    Update Accumulator.java

--- Accumulator.java
+++ Accumulator.java
@@ -1,4 +1,4 @@
 @FunctionalInterface
 public interface Accumulator<P, T> {
-    void accumulate(P collection, T value);
+    void accumulate(P collection, T value); //Метод для добавления элемента в коллекцию
 }
